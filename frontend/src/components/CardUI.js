929f386608a57a08b0097d634327a7fe075a295b
Lostmaniac9 <jdog2349@gmail.com>
2021-11-03 09:53:52 -0400

    addedd from MERN A up to adding MongoDB functionality

--- frontend/src/components/CardUI.js
+++ frontend/src/components/CardUI.js
@@ -1,26 +1,69 @@
 import React from 'react';
 import React, { useState } from 'react';
+
 function CardUI()
 {
-    var card = '';
-    var search = '';
-    const [message,setMessage] = useState('');
-    const [searchResults,setResults] = useState('');
-    const [cardList,setCardList] = useState('');
-    
-    const addCard = async event => 
-    {
-        event.preventDefault();
-         alert('addCard() ' + card.value);
-    }; // this is a random comment
-
-    const searchCard = async event => 
-    {
-        event.preventDefault();
-        alert('searchCard() ' + search.value);
-    };
+  var _ud = localStorage.getItem('user_data');
+  var ud = JSON.parse(_ud);
+  var userId = ud.id;
+  var firstName = ud.firstName;
+  var lastName = ud.lastName;
+  const addCard = async event => 
+  {
+  event.preventDefault();
+      var obj = {userId:userId,card:card.value};
+      var js = JSON.stringify(obj);
+      try
+      {
+          const response = await fetch('http://localhost:5000/api/addcard', {method:'POST',body:js,headers:{'Content-Type':'application/json'}});
+          var txt = await response.text();
+          var res = JSON.parse(txt);
+          if( res.error.length > 0 )
+          {
+              setMessage( "API Error:" + res.error );
+          }
+          else
+          {
+              setMessage('Card has been added');
+          }
+      }
+      catch(e)
+      {
+          setMessage(e.toString());
+      }
+};
+  const searchCard = async event => 
+  {
+      event.preventDefault();
+      
+      var obj = {userId:userId,search:search.value};
+      var js = JSON.stringify(obj);
+      try
+      {
+          const response = await fetch('http://localhost:5000/api/searchcards', {method:'POST',body:js,headers:{'Content-Type':'application/json'}});
+          var txt = await response.text();
+          var res = JSON.parse(txt);
+          var _results = res.results;
+          var resultText = '';
+          for( var i=0; i<_results.length; i++ )
+          {
+              resultText += _results[i];
+              if( i < _results.length - 1 )
+              {
+                  resultText += ', ';
+              }
+          }
+          setResults('Card(s) have been retrieved');
+          setCardList(resultText);
+      }
+      catch(e)
+      {
+          alert(e.toString());
+          setResults(e.toString());
+      }
+  };
     return(
-        <div id="cardUIDiv">
+      <div id="cardUIDiv">
         <br />
         <input type="text" id="searchText" placeholder="Card To Search For" 
           ref={(c) => search = c} />
@@ -33,7 +76,7 @@ function CardUI()
         <button type="button" id="addCardButton" class="buttons" 
           onClick={addCard}> Add Card </button><br />
         <span id="cardAddResult">{message}</span>
-      </div>
+    </div>
     );
 }
 export default CardUI;
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import React, { useState } from 'react';
+
 function Login()
 {
   var loginName;
   var loginPassword;
   const [message,setMessage] = useState('');
   const doLogin = async event => 
-  {
-      event.preventDefault();
-      alert('doIt() ' + loginName.value + ' ' + loginPassword.value );
-  };
+    {
+        event.preventDefault();
+        var obj = {login:loginName.value,password:loginPassword.value};
+        var js = JSON.stringify(obj);
+        try
+        {    
+            const response = await fetch('http://localhost:5000/api/login', {method:'POST',body:js,headers:{'Content-Type':'application/json'}});
+            var res = JSON.parse(await response.text());
+            if( res.id <= 0 )
+            {
+                setMessage('User/Password combination incorrect');
+            }
+            else
+            {
+                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+                localStorage.setItem('user_data', JSON.stringify(user));
+                setMessage('');
+                window.location.href = '/cards';
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }    
+    };
     return(
       <div id="loginDiv">
         <form onSubmit={doLogin}>
@@ -20,7 +44,7 @@ function Login()
         <input type="submit" id="loginButton" class="buttons" value = "Do It"
           onClick={doLogin} />
         </form>
-        <span id="loginResult"></span>
+        <span id="loginResult">{message}</span>
      </div>
     );
 };
