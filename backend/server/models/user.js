cbbe0fd3cac76b1d88787a32d4892296090dc2c6
kdcrist <56696111+kdcrist@users.noreply.github.com>
2021-11-06 23:21:09 -0400

    create user.js

--- backend/server/models/user.js
+++ backend/server/models/user.js
@@ -1,1 +1,30 @@
-create
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: ''
+  },
+  email: {
+    type: String,
+    default: ''
+  },
+  password: {
+    type: String,
+    default: ''
+  },
+  verified: {
+    type: Boolean,
+    default: true
+  }
+});
+
+UserSchema.methods.generateHash = function(passwordhash) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password) {
+   return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
