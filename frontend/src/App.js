ff1c17992ef396f02d5b7c26183314cd5b2bb130
Lostmaniac9 <jdog2349@gmail.com>
2021-11-08 15:30:59 -0500

    Added page for form filling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
@@ -18,4 +18,21 @@ function App() {
     </Router>
   );
 }
-export default App;
+export default App;*/
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form.js';class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Form Validation Demo</h2>
+        </div>
+        <Form />
+      </div>
+    );
+  }
+}export default App;
